e509faed9f575f9aa8b346fc684f9a85af38d292
genius <129747209+turic24@users.noreply.github.com>
2025-04-09 22:13:19 +0700

    tma code file

---
+++ code/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="min-h-screen flex flex-col justify-center items-center text-center"
+  >
+    <ClientOnly>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </ClientOnly>
+  </div>
+</template>
---
+++ code/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-4 text-center text-lg">
+    Hello from Mini App! <br />
+    Developer username: <strong>@Tuptuppp</strong>
+  </div>
+  <h1>For Round 2 for Ton Contest</h1>
+  <script>
+    export default {
+      mounted() {
+        if (window.Telegram.WebApp) {
+          window.Telegram.WebApp.ready();
+          window.Telegram.WebApp.toggleFullscreen();
+        }
+      },
+    };
+  </script>
+</template>
